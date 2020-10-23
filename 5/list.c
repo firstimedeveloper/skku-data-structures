c95a9fbd9556dbd9e4a95429aaaee94e569c7903
Jun Hyuk Han <hanjun.1998@gmail.com>
2020-10-23 14:35:01 +0900

    .

--- 5/list.c
+++ 5/list.c
@@ -85,7 +85,7 @@ int main() {
         fgets(line, 8, stdin);
         int ret = sscanf(line, "%s %c\n", op, &let);
         if (strcmp(op, "erase") == 0) {
-            if (cursor != 0 || l.len != 0) {    
+            if (cursor != 0 && l.len != 0) {    
                 RemovePos(&l, --cursor);
             }
         }
@@ -95,13 +95,14 @@ int main() {
                     let = ' ';
                 InsertPos(&l, cursor, let);
                 cursor++;
-            } else if (strcmp(op, "move") == 0) {
-                if (let == 'l')
+            } else if (strcmp(op, "move") == 0 && l.len > 0) {
+                if (let == 'l') {
                     if (l.len - cursor < l.len) 
                         cursor--;
-                else if (let == 'r')
-                    if (l.len + cursor > 0)
+                } else if (let == 'r') {
+                    if (l.len > cursor)
                         cursor++;
+                }
             }
         }
             
